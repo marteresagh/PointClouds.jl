1742f432c32656232731820860071aac22af3c35
marteresagh <marteresa28@gmail.com>
2020-01-15 08:28:21 +0100

    fix examples: alphashapes

---
+++ examples/AlphaShapes/Examples.jl
@@ -0,0 +1,59 @@
+using LinearAlgebraicRepresentation, AlphaStructures
+Lar = LinearAlgebraicRepresentation
+using PointClouds
+using ViewerGL
+GL=ViewerGL
+include("../viewfunction.jl")
+
+# from this repository
+fname = "examples/AlphaShapes/pointCloud/CAVA/r.las"
+# fname = "examples/AlphaShapes/pointCloud/CUPOLA/r.las"
+# fname = "examples/AlphaShapes/pointCloud/SCALE/r.las"
+
+Voriginal,VV,rgb = PointClouds.loadlas(fname)
+_,V = PointClouds.subtractaverage(Voriginal)
+
+
+GL.VIEW(
+	[
+		colorview(V,VV,rgb)
+	]
+);
+
+DT = PointClouds.mat3DT(V)
+filtration = AlphaStructures.alphaFilter(V, DT);
+
+α = 0.0467882
+VV, EV, FV, TV = AlphaStructures.alphaSimplex(V, filtration, α);
+
+GL.VIEW(
+	[
+		#colorview(V,TV,rgb);
+		colorview(V,FV,rgb)
+	]
+);
+
+
+#=
+open("z.txt", "w") do f
+	n=size(V,2)
+	write(f, "$n \n")
+	for i in 1:n
+		x=V[1,i]
+		y=V[2,i]
+		z=V[3,i]
+		write(f, "$z \n")
+	end
+end
+=#
+
+
+# using Distributed
+# addprocs(8)
+#
+# @everywhere using Pkg
+# @everywhere Pkg.activate("./AlphaStructures")
+#
+# @everywhere using  AlphaStructures
+#
+# DT = AlphaStructures.delaunayTriangulation(V);
---
+++ examples/AlphaShapes/LocalRep.jl
@@ -0,0 +1,47 @@
+using LinearAlgebraicRepresentation, AlphaStructures
+Lar = LinearAlgebraicRepresentation
+using PointClouds
+using ViewerGL
+GL=ViewerGL
+include("../viewfunction.jl")
+
+# from my local repository
+fname = "C:\\Users\\marte\\Documents\\potreeDirectory\\pointclouds\\DIGA"
+allfile = PointClouds.filelevel(fname,1)
+_,_,_,_,_,spacing = PointClouds.readJSON(fname)
+
+Voriginal,VV,rgb = PointClouds.loadlas(allfile...)
+_,V = PointClouds.subtractaverage(Voriginal)
+
+GL.VIEW(
+	[
+		colorview(V,VV,rgb)
+	]
+);
+
+
+DT = PointClouds.mat3DT(V)
+filtration = AlphaStructures.alphaFilter(V, DT);
+
+α = 0.5 #da variare
+VV, EV, FV, TV = AlphaStructures.alphaSimplex(V, filtration, α);
+
+GL.VIEW(
+	[
+		colorview(V,FV,rgb)
+	]
+);
+
+#=
+open("DT.jl", "w") do f
+	write(f, "[")
+	for simplex in DT
+		write(f, "[")
+		for i in simplex
+    		write(f, "$i,")
+		end
+		write(f, "],")
+	end
+	write(f, "]")
+end
+=#
