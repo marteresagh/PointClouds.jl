9ef4e5e6dd9ba1ce7d07c0390dad7376adb52291
marteresagh <marteresa28@gmail.com>
2020-02-11 10:23:31 +0100

    printmodel

---
+++ src/printmodel.jl
@@ -0,0 +1,34 @@
+function modelprint(V,CV)
+
+	open("model.jl", "w") do f
+		n=size(V,2)
+		write(f, "V=[")
+		for i in 1:n
+			x=V[1,i]
+			write(f, "$x ")
+		end
+		write(f, "; ")
+		for i in 1:n
+			y=V[2,i]
+			write(f, "$y ")
+		end
+		write(f, "; ")
+		for i in 1:n
+			z=V[3,i]
+			write(f, "$z ")
+		end
+		write(f, "]\n")
+
+		write(f, "\n ")
+		write(f, "CV=[")
+
+		for simplex in CV
+			write(f, "[")
+			for i in simplex
+				write(f, "$i,")
+			end
+			write(f, "],")
+		end
+		write(f, "]")
+	end
+end
