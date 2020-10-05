2762c99eabfa578c3734c7a7cf52a75ad68788db
marteresagh <marteresa28@gmail.com>
2020-10-05 16:23:22 +0200

    some useful struct to define

---
+++ src/struct.jl
@@ -0,0 +1,31 @@
+mutable struct Plane
+    normal::Array{Float64,1}
+    centroid::Array{Float64,1}
+end
+
+struct PlaneDetected
+    points::Lar.Points
+    plane::Plane
+end
+
+mutable struct AxisAlignedBoundingBox
+    x_max::Float64
+    x_min::Float64
+    y_max::Float64
+    y_min::Float64
+    z_max::Float64
+    z_min::Float64
+end
+
+struct CloudMetadata
+    version::String
+    octreeDir::String
+    projection::String
+    points::Int64
+    boundingBox::AxisAlignedBoundingBox
+    tightBoundingBox::AxisAlignedBoundingBox
+    pointAttributes::String
+    spacing::Float64
+    scale::Float64
+    hierarchyStepSize::Int32
+end
